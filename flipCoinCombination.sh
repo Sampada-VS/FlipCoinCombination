de54c24fd68d07aa3d90d6bb831dc01b1619e864
Sampada-VS <samssvv1@gmail.com>
2020-08-06 02:08:32 +0530

    Adding feature-store singlet coin combo and display win %

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,23 @@
 #!/bin/bash -x
 H=1
-flip_coin=$((RANDOM%2))
+NUM_OF_FLIPS=30
 
-if [ $flip_coin -eq $H ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+h_win=0
+t_win=0
+
+declare -A singletDict
+for (( i=1; i<=$NUM_OF_FLIPS; i++ ))
+do
+   flip_coin=$((RANDOM%2))
+   case $flip_coin in
+		$H)	singletDict[$i]="H"
+				h_win=$(($h_win+1))
+				;;
+		*)	singletDict[$i]="T"
+			t_win=$(($t_win+1))
+			;;
+	esac
+done
+echo ${singletDict[@]}
+percent_h=`awk "BEGIN { print $h_win/$NUM_OF_FLIPS*100 }"`
+percent_t=`awk "BEGIN { print $t_win/$NUM_OF_FLIPS*100 }"`
