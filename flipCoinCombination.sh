b1eb3ebd1f2e9a0c8efe16ad71ecc38f5fec5050
Sampada-VS <samssvv1@gmail.com>
2020-08-06 02:31:25 +0530

    Adding-store triplet combo in dict and show win %

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,40 +1,65 @@
 #!/bin/bash -x
-H_H=1
-H_T=2
-T_H=3
+H_H_H=1
+H_H_T=2
+H_T_H=3
+T_H_H=4
+H_T_T=5
+T_T_H=6
+T_H_T=7
+
 NUM_OF_FLIPS=30
 
-h_h_win=0
-h_t_win=0
-t_h_win=0
-t_t_win=0
+h_h_h_win=0
+h_h_t_win=0
+h_t_h_win=0
+t_h_h_win=0
+h_t_t_win=0
+t_t_h_win=0
+t_h_t_win=0
+t_t_t_win=0
 
-declare -A doubletDict
+declare -A tripletDict
 declare -A percentDict
 
 for (( i=1; i<=$NUM_OF_FLIPS; i++ ))
 do
-	flip_coin=$((RANDOM%4))
+	flip_coin=$((RANDOM%8))
 	case $flip_coin in
-		$H_H)	doubletDict[$i]="HH"
-				h_h_win=$(($h_h_win+1))
-				;;
-		$H_T)	doubletDict[$i]="HT"
-				h_t_win=$(($h_t_win+1))
-				;;
-		$T_H)	doubletDict[$i]="TH"
-				t_h_win=$(($t_h_win+1))
-				;;
-		*)	doubletDict[$i]="TT"
-			t_t_win=$(($t_t_win+1))
+		$H_H_H)	tripletDict[$i]="HHH"
+					h_h_h_win=$(($h_h_h_win+1))
+					;;
+		$H_H_T)	tripletDict[$i]="HHT"
+					h_h_t_win=$(($h_h_t_win+1))
+					;;
+		$H_T_H)	tripletDict[$i]="HTH"
+					h_t_h_win=$(($h_t_h_win+1))
+					;;
+		$T_H_H)	tripletDict[$i]="THH"
+					t_h_h_win=$(($t_h_h_win+1))
+					;;
+		$H_T_T)	tripletDict[$i]="HTT"
+					h_t_t_win=$(($h_t_t_win+1))
+					;;
+		$T_T_H)	tripletDict[$i]="TTH"
+					t_t_h_win=$(($t_t_h_win+1))
+					;;
+		$T_H_T)	tripletDict[$i]="THT"
+					t_h_t_win=$(($t_h_t_win+1))
+					;;
+		*)	tripletDict[$i]="TTT"
+			t_t_t_win=$(($t_t_t_win+1))
 			;;
 	esac
 done
-echo ${doubletDict[@]}
-percentDict[HH]=`awk "BEGIN { print $h_h_win/$NUM_OF_FLIPS*100 }"`
-percentDict[HT]=`awk "BEGIN { print $h_t_win/$NUM_OF_FLIPS*100 }"`
-percentDict[TH]=`awk "BEGIN { print $t_h_win/$NUM_OF_FLIPS*100 }"`
-percentDict[TT]=`awk "BEGIN { print $t_t_win/$NUM_OF_FLIPS*100 }"`
+echo "Triplet combination " ${tripletDict[@]}
+percentDict[HHH]=`awk "BEGIN { print $h_h_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[HHT]=`awk "BEGIN { print $h_h_t_win/$NUM_OF_FLIPS*100 }"`
+percentDict[HTH]=`awk "BEGIN { print $h_t_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[THH]=`awk "BEGIN { print $t_h_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[HTT]=`awk "BEGIN { print $h_t_t_win/$NUM_OF_FLIPS*100 }"`
+percentDict[TTH]=`awk "BEGIN { print $t_t_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[THT]=`awk "BEGIN { print $t_h_t_win/$NUM_OF_FLIPS*100 }"`
+percentDict[TTT]=`awk "BEGIN { print $t_t_t_win/$NUM_OF_FLIPS*100 }"`
 
-echo ${!percentDict[@]}
-echo ${percentDict[@]}
+echo "Triplet keys " ${!percentDict[@]}
+echo "Triplet combo percentage " ${percentDict[@]}
