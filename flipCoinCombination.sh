1e001ee56b9555fded5cbad9c93430c23a3331a9
Sampada-VS <samssvv1@gmail.com>
2020-08-06 02:17:01 +0530

    Adding-store doublet coin combo in dictionary and display wplay win %

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,23 +1,40 @@
 #!/bin/bash -x
-H=1
+H_H=1
+H_T=2
+T_H=3
 NUM_OF_FLIPS=30
 
-h_win=0
-t_win=0
+h_h_win=0
+h_t_win=0
+t_h_win=0
+t_t_win=0
 
-declare -A singletDict
+declare -A doubletDict
+declare -A percentDict
+
 for (( i=1; i<=$NUM_OF_FLIPS; i++ ))
 do
-   flip_coin=$((RANDOM%2))
-   case $flip_coin in
-		$H)	singletDict[$i]="H"
-				h_win=$(($h_win+1))
+	flip_coin=$((RANDOM%4))
+	case $flip_coin in
+		$H_H)	doubletDict[$i]="HH"
+				h_h_win=$(($h_h_win+1))
 				;;
-		*)	singletDict[$i]="T"
-			t_win=$(($t_win+1))
+		$H_T)	doubletDict[$i]="HT"
+				h_t_win=$(($h_t_win+1))
+				;;
+		$T_H)	doubletDict[$i]="TH"
+				t_h_win=$(($t_h_win+1))
+				;;
+		*)	doubletDict[$i]="TT"
+			t_t_win=$(($t_t_win+1))
 			;;
 	esac
 done
-echo ${singletDict[@]}
-percent_h=`awk "BEGIN { print $h_win/$NUM_OF_FLIPS*100 }"`
-percent_t=`awk "BEGIN { print $t_win/$NUM_OF_FLIPS*100 }"`
+echo ${doubletDict[@]}
+percentDict[HH]=`awk "BEGIN { print $h_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[HT]=`awk "BEGIN { print $h_t_win/$NUM_OF_FLIPS*100 }"`
+percentDict[TH]=`awk "BEGIN { print $t_h_win/$NUM_OF_FLIPS*100 }"`
+percentDict[TT]=`awk "BEGIN { print $t_t_win/$NUM_OF_FLIPS*100 }"`
+
+echo ${!percentDict[@]}
+echo ${percentDict[@]}
